2b110ae6210581e01475f5d80fa8f39339c37fba
ali <asek_23.02.09@mail.ru>
2026-07-06 22:09:28 +0500

    Added function and interactive menu

--- department/script.sh
+++ department/script.sh
@@ -9,6 +9,7 @@ if [ "$(id -u)" != 0 ]; then
     exit 1
 fi
 
+# greetings
 cat <<EOF
 Welcome!
 Read the department groups below
@@ -17,9 +18,9 @@ sec group = cybersecurity team (tcpdump, journalctl)
 lead group = team leads (full root access)
 EOF
 
-read -p "Print username: " USER_1
-read -p "Print groupname: " GROUP_1
 
+# function
+create_user() {
 if ! getent group "$GROUP_1" &>/dev/null; then # getent prevents if group exists
     groupadd "$GROUP_1"
 fi
@@ -63,4 +64,39 @@ else
     echo "$USER_1 already exists!" >&2
     exit 1
 fi
+}
 
+
+if ! [ -z "echo ./users" ]; then
+  for line in $(cat ./users); do
+  USER_1=$(echo $line | cut -d' ' -f1)
+  GROUP_1=$(echo $line | cut -d' ' -f2)
+  create_user; done
+else
+  read -p "Print username: " USER_1
+  read -p "Print groupname: " GROUP_1
+  create_user
+fi
+
+# Interactive selection (select, case)
+select number in "Add new user from ./users" "Add new user interactively" "Show last 5 users" "Show last 5 groups" "Exit"; do
+case $number in
+  "Add new user from ./users")
+                if [ -z $(cat ./users &> /dev/null) ]; then
+                   echo "Undefined variables, check your ./users file"
+                else
+                   for line in $(cat ./users); do
+                   USER_1=$(echo $line | cut -d' ' -f1)
+                   GROUP_1=$(echo $line | cut -d' ' -f2)
+                   create_user; done
+                fi ;;
+  "Add new user interactively")
+                  read -p "Print username: " USER_1
+                  read -p "Print groupname: " GROUP_1
+                  create_user ;;
+  "Show last 5 users") tail -n 5 /etc/passwd ;;
+  "Show last 5 groups") tail -n 5 /etc/group ;;
+  "Exit") break ;;
+  *) echo Wrong option ;;
+esac
+done
